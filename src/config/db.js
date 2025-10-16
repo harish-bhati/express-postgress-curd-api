8539cde890e58e495e47b632c7eed7fa834f6479
Harish Bhati <pmaithaniex2@peci.com>
2025-10-16 14:22:47 +0530

    env and db connect file updates

--- src/config/db.js
+++ src/config/db.js
@@ -3,21 +3,20 @@ const { Pool } = pkg;
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log("user",process.env.DB_USER);
-console.log("host",process.env.DB_HOST);
-console.log("database",process.env.DB_DATABASE);
-console.log("pass",process.env.DB_PASSWORD);
-console.log("port",process.env.DB_PORT);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_DATABASE,
+//   password: process.env.DB_PASSWORD,
+//   port: Number(process.env.DB_PORT)
+// });
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,   
-  database: process.env.DB_DATABASE,     
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
-
-pool.on('connect', () => {  
-    console.log('Connected to the database');
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
